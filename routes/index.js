3f2d2837b5920bbbfd6a8e6bfae20730868a6164
Prasad Sawant <prasad.b.sawant@ericsson.com>
2019-01-29 15:41:42 -0800

    logging

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  
+  console.log(req.body);
   res.status(200).json({ "message": "ok" });
 });
 
 router.get('/', function(req, res, next) {
+  console.log(req.body);
   res.status(200).json({ "message": "ok" });
 });
 
