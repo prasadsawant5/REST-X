fa733fe4a49ee6d3a98c6f561288f278a9bdc454
Prasad Sawant <prasad.b.sawant@ericsson.com>
2019-01-29 16:35:53 -0800

    get endpoint removed

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  res.status(200).json({ "message": "ok" });
-});
-
-router.get('/', function(req, res, next) {
-  console.log(req.body);
+  console.log(req.body.request);
+  console.log(req.body.request.intent.slots);
   res.status(200).json({ "message": "ok" });
 });
 
