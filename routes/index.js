5a27174c62f10b39b9f69439126204b885a09359
Prasad Sawant <prasad.b.sawant@ericsson.com>
2019-02-07 17:29:59 -0800

    ws removed

--- routes/index.js
+++ routes/index.js
@@ -12,56 +12,56 @@ const ACCESS_TOKEN = 'https://api.amazon.com/auth/o2/token';
 var accessToken;
 var refreshToken;
 
-var server = http.createServer(function(request, response) {
-  console.log((new Date()) + ' Received request for ' + request.url);
-  response.writeHead(404);
-  response.end();
-});
+// var server = http.createServer(function(request, response) {
+//   console.log((new Date()) + ' Received request for ' + request.url);
+//   response.writeHead(404);
+//   response.end();
+// });
 
-server.listen(8093, function() {
-  console.log((new Date()) + ' Server is listening on port 8093');
-});
+// server.listen(8093, function() {
+//   console.log((new Date()) + ' Server is listening on port 8093');
+// });
 
-wsServer = new WebSocketServer({
-  httpServer: server,
-  // You should not use autoAcceptConnections for production
-  // applications, as it defeats all standard cross-origin protection
-  // facilities built into the protocol and the browser.  You should
-  // *always* verify the connection's origin and decide whether or not
-  // to accept it.
-  autoAcceptConnections: false
-});
+// wsServer = new WebSocketServer({
+//   httpServer: server,
+//   // You should not use autoAcceptConnections for production
+//   // applications, as it defeats all standard cross-origin protection
+//   // facilities built into the protocol and the browser.  You should
+//   // *always* verify the connection's origin and decide whether or not
+//   // to accept it.
+//   autoAcceptConnections: false
+// });
 
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
+// function originIsAllowed(origin) {
+//   // put logic here to detect whether the specified origin is allowed.
+//   return true;
+// }
 
-wsServer.on('request', function(request) {
-  if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-  }
+// wsServer.on('request', function(request) {
+//   if (!originIsAllowed(request.origin)) {
+//       // Make sure we only accept requests from an allowed origin
+//       request.reject();
+//       console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+//       return;
+//   }
 
-  var connection = request.accept('echo-protocol', request.origin);
-  console.log((new Date()) + ' Connection accepted.');
-  console.log(request.httpRequest.headers);
-  connection.on('message', function(message) {
-      if (message.type === 'utf8') {
-          console.log('Received Message: ' + message.utf8Data);
-          connection.sendUTF(message.utf8Data);
-      }
-      else if (message.type === 'binary') {
-          console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-          connection.sendBytes(message.binaryData);
-      }
-  });
-  connection.on('close', function(reasonCode, description) {
-      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-  });
-});
+//   var connection = request.accept('echo-protocol', request.origin);
+//   console.log((new Date()) + ' Connection accepted.');
+//   console.log(request.httpRequest.headers);
+//   connection.on('message', function(message) {
+//       if (message.type === 'utf8') {
+//           console.log('Received Message: ' + message.utf8Data);
+//           connection.sendUTF(message.utf8Data);
+//       }
+//       else if (message.type === 'binary') {
+//           console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+//           connection.sendBytes(message.binaryData);
+//       }
+//   });
+//   connection.on('close', function(reasonCode, description) {
+//       console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+//   });
+// });
 
 router.post('/', function(req, res, next) {
   var r = {};
@@ -80,7 +80,7 @@ router.post('/', function(req, res, next) {
       }
     };
 
-    connection.sendUTF(JSON.stringify({ test: 'test' }));
+    // connection.sendUTF(JSON.stringify({ test: 'test' }));
   }
 
   res.status(200).json(r);
