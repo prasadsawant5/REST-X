c89a82fb125845b00c5daf20512b0906a6f8218b
Prasad Sawant <prasad.b.sawant@ericsson.com>
2019-02-07 16:03:12 -0800

    ws

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const rp = require('request-promise');
-const http = require('http');
-const WebSocketServer = require('websocket').server;
 
 var router = express.Router();
 
@@ -11,55 +9,6 @@ const ACCESS_TOKEN = 'https://api.amazon.com/auth/o2/token';
 
 var accessToken;
 var refreshToken;
-
-var server = http.createServer(function(request, response) {
-  console.log((new Date()) + ' Received request for ' + request.url);
-  response.writeHead(404);
-  response.end();
-});
-server.listen(8093, function() {
-  console.log((new Date()) + ' Server is listening on port 8093');
-});
-
-wsServer = new WebSocketServer({
-  httpServer: server,
-  // You should not use autoAcceptConnections for production
-  // applications, as it defeats all standard cross-origin protection
-  // facilities built into the protocol and the browser.  You should
-  // *always* verify the connection's origin and decide whether or not
-  // to accept it.
-  autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-// put logic here to detect whether the specified origin is allowed.
-return true;
-}
-
-wsServer.on('request', function(request) {
-  if (!originIsAllowed(request.origin)) {
-    // Make sure we only accept requests from an allowed origin
-    request.reject();
-    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-    return;
-  }
-  
-  var connection = request.accept('echo-protocol', request.origin);
-  console.log((new Date()) + ' Connection accepted.');
-  connection.on('message', function(message) {
-      if (message.type === 'utf8') {
-          console.log('Received Message: ' + message.utf8Data);
-          connection.sendUTF(message.utf8Data);
-      }
-      else if (message.type === 'binary') {
-          console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-          connection.sendBytes(message.binaryData);
-      }
-  });
-  connection.on('close', function(reasonCode, description) {
-      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-  });
-});
 
 router.post('/', function(req, res, next) {
   var r = {};
